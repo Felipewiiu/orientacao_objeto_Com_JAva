8ab4bad6c171178ad4eb68bcfb5d2a2329d45f1f
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-10-05 14:19:17 -0300

    Criando atributos privados

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Filme {
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluesoNoPlano;
+    private double somaAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinuto;
+
+    int getTotalDeAvaliacoes () {
+        return totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica () {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinuto);
+    }
+    
+    void avalia(double nota) {
+        somaAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia () {
+        return somaAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        // nesse caso "Filme" seria o tipo da variável
+        // Esse recurso é chamado de tipo por referência
+
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 2023;
+        meuFilme.duracaoEmMinuto = 180;
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8.0);
+        meuFilme.avalia(5.0);
+        meuFilme.avalia(10);
+
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+
+
+
+
+
+
+
+    }
 }
